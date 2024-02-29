29d124b7bdd16dcaa143d130af4597866406aa77
munna96 <97169099+munna96@users.noreply.github.com>
2024-02-29 19:13:19 +0530

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -4,12 +4,12 @@ resource "aws_instance" "mySonarInstance" {
       instance_type = "t2.micro"
       vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
       tags= {
-        Name = "sonar_instance"
+        Name = "msuh_sonar_instance"
       }
     }
 
  resource "aws_security_group" "sonar-sg-2022" {
-      name        = "security_sonar_group_2022"
+      name        = "security_sonar_group_2022_msuh"
       description = "security group for Sonar"
 
       ingress {
@@ -35,7 +35,7 @@ resource "aws_instance" "mySonarInstance" {
       }
 
       tags= {
-        Name = "security_sonar"
+        Name = "security_sonar_msuh"
       }
     }
 
@@ -44,6 +44,6 @@ resource "aws_eip" "mySonarInstance" {
   vpc      = true
   instance = aws_instance.mySonarInstance.id
 tags= {
-    Name = "sonar_elastic_ip"
+    Name = "sonar_elastic_ip_msuh"
   }
 }
