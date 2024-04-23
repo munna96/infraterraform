8a2819d8db0159335cf0baeea636516ca841ed81
munna96 <97169099+munna96@users.noreply.github.com>
2024-04-23 11:00:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "sherlok-s3bucket-" #you have to change the bucket name 
+    default     = "jenkinmush" #you have to change the bucket name 
 }
 variable "tags" {
     type        = map
