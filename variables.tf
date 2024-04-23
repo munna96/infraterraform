f61a20b089af3b52a4ae29c01764af9293d55a53
munna96 <97169099+munna96@users.noreply.github.com>
2024-04-23 13:23:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "jenkinmush" #you have to change the bucket name 
+    default     = "mynewbucket" #you have to change the bucket name 
 }
 variable "tags" {
     type        = map
